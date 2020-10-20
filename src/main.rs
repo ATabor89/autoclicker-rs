f2533f49e2b06b7ecea0d50cbff65744a95a641a
Aaron Tabor <Shoggoth269@gmail.com>
2020-10-19 23:37:09 -0500

    Added middle and right clicking

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #[cfg(windows)] extern crate winapi;
 use std::io::Error;
 
+// EDIT TIME BETWEEN CLICKS HERE
 static SLEEP_BETWEEN_CLICKS: std::time::Duration = std::time::Duration::from_millis(50);
+
+// LEAVE THIS ONE ALONE
+// Sleep keeps the loop from killing too much CPU time when idle
 static SLEEP_WHEN_WAITING: std::time::Duration = std::time::Duration::from_millis(50);
 
 #[cfg(windows)]
@@ -28,6 +32,52 @@ fn left_click() -> Result<(), Error> {
 }
 
 #[cfg(windows)]
+fn right_click() -> Result<(), Error> {
+    use winapi::um::winuser::{INPUT, INPUT_u, SendInput, MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_RIGHTUP, INPUT_MOUSE};
+
+    let mut input = INPUT {
+        type_: INPUT_MOUSE,
+        u: INPUT_u::default(),
+    };
+    let mut mouse_input = unsafe { input.u.mi_mut() };
+    mouse_input.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+    unsafe {
+        SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
+    }
+
+    let mut mouse_input = unsafe { input.u.mi_mut() };
+    mouse_input.dwFlags = MOUSEEVENTF_RIGHTUP;
+    unsafe {
+        SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
+    }
+
+    Ok(())
+}
+
+#[cfg(windows)]
+fn middle_click() -> Result<(), Error> {
+    use winapi::um::winuser::{INPUT, INPUT_u, SendInput, MOUSEEVENTF_MIDDLEDOWN, MOUSEEVENTF_MIDDLEUP, INPUT_MOUSE};
+
+    let mut input = INPUT {
+        type_: INPUT_MOUSE,
+        u: INPUT_u::default(),
+    };
+    let mut mouse_input = unsafe { input.u.mi_mut() };
+    mouse_input.dwFlags = MOUSEEVENTF_MIDDLEDOWN;
+    unsafe {
+        SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
+    }
+
+    let mut mouse_input = unsafe { input.u.mi_mut() };
+    mouse_input.dwFlags = MOUSEEVENTF_MIDDLEUP;
+    unsafe {
+        SendInput(1, &mut input, std::mem::size_of::<INPUT>() as i32);
+    }
+
+    Ok(())
+}
+
+#[cfg(windows)]
 fn key_listener() -> Result<(), Error> {
     use winapi::um::winuser::{GetAsyncKeyState, VK_F6, VK_F7};
 
@@ -44,6 +94,8 @@ fn key_listener() -> Result<(), Error> {
 
         if click {
             left_click().unwrap();
+            right_click().unwrap();
+            middle_click().unwrap();
             std::thread::sleep(SLEEP_BETWEEN_CLICKS);
         }
         else {
