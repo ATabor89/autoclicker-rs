ddaf60e32f470a97c7835b52b837584d8fee5bee
Aaron Tabor <Shoggoth269@gmail.com>
2020-10-20 15:03:42 -0500

    Added live config file and uploading binaries of newest version

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,89 @@
 #[cfg(windows)] extern crate winapi;
 use std::io::Error;
+use std::collections::HashMap;
 
-// EDIT TIME BETWEEN CLICKS HERE
-static SLEEP_BETWEEN_CLICKS: std::time::Duration = std::time::Duration::from_millis(50);
+use config::*;
+use notify::{DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
+use serde::{Deserialize, Serialize};
+use std::sync::mpsc::channel;
+use std::sync::RwLock;
+
+#[macro_use]
+extern crate lazy_static;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Settings {
+    time_between_clicks_millis: u64,
+    should_left_click: bool,
+    should_middle_click: bool,
+    should_right_click: bool,
+}
+
+lazy_static! {
+    static ref SETTINGS: RwLock<Config> = RwLock::new({
+        let mut settings = Config::default();
+        settings.merge(File::with_name("Settings.toml")).unwrap();
+
+        settings
+    });
+}
+
+fn show() {
+    println!(
+        " * Settings :: \n\x1b[31m{:?}\x1b[0m",
+        SETTINGS
+            .read()
+            .unwrap()
+            .clone()
+            .try_into::<HashMap<String, String>>()
+            .unwrap()
+    );
+}
+
+fn get_settings() -> Settings {
+    let settings = SETTINGS
+        .read()
+        .unwrap()
+        .clone()
+        .try_into::<Settings>()
+        .unwrap();
+
+    settings
+}
+
+fn watch() {
+    // Create a channel to receive the events.
+    let (tx, rx) = channel();
+
+    // Automatically select the best implementation for your platform.
+    // You can also access each implementation directly e.g. INotifyWatcher.
+    let mut watcher: RecommendedWatcher =
+        Watcher::new(tx, std::time::Duration::from_secs(2)).unwrap();
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher
+        .watch("./Settings.toml", RecursiveMode::NonRecursive)
+        .unwrap();
+
+    // This is a simple loop, but you may want to use more complex logic here,
+    // for example to handle I/O.
+    loop {
+        match rx.recv() {
+            Ok(DebouncedEvent::Write(_)) => {
+                println!(" * Settings.toml written; refreshing configuration ...");
+                SETTINGS.write().unwrap().refresh().unwrap();
+                show();
+            }
+
+            Err(e) => println!("watch error: {:?}", e),
+
+            _ => {
+                // Ignore event
+            }
+        }
+    }
+}
 
 // LEAVE THIS ONE ALONE
 // Sleep keeps the loop from killing too much CPU time when idle
@@ -81,10 +162,17 @@ fn middle_click() -> Result<(), Error> {
 fn key_listener() -> Result<(), Error> {
     use winapi::um::winuser::{GetAsyncKeyState, VK_F6, VK_F7};
 
+    println!("Autoclicker started. Press F6 to toggle clicking and press F7 to terminate program.");
+    println!("Modify Settings.toml to change configuration (can be done while program is live).");
+
     let mut click = false;
+    let mut settings = get_settings();
 
     loop {
         if unsafe {(GetAsyncKeyState(VK_F6) & 1) != 0} {
+            if !click {
+                settings = get_settings();
+            }
             click = !click;
         }
 
@@ -93,10 +181,16 @@ fn key_listener() -> Result<(), Error> {
         }
 
         if click {
-            left_click().unwrap();
-            right_click().unwrap();
-            middle_click().unwrap();
-            std::thread::sleep(SLEEP_BETWEEN_CLICKS);
+            if settings.should_left_click {
+                left_click().unwrap();
+            }
+            if settings.should_middle_click {
+                middle_click().unwrap();
+            }
+            if settings.should_right_click {
+                right_click().unwrap();
+            }
+            std::thread::sleep(std::time::Duration::from_millis(settings.time_between_clicks_millis));
         }
         else {
             std::thread::sleep(SLEEP_WHEN_WAITING);
@@ -107,5 +201,9 @@ fn key_listener() -> Result<(), Error> {
 }
 
 fn main() {
+    std::thread::spawn(|| {
+        watch();
+    });
+
     key_listener().unwrap();
 }
